d441666bbc445f5461eed86153a909c134151d86
hirata4376-sss <hirata4376@gmail.com>
2026-06-27 21:08:07 +0900

    update

--- GitHubに送る.bat
+++ GitHubに送る.bat
@@ -5,7 +5,7 @@ echo ===== %date% %time% ===== > git_log.txt
 if exist .git\index.lock del /f .git\index.lock >> git_log.txt 2>&1
 git add . >> git_log.txt 2>&1
 git commit -m "update" >> git_log.txt 2>&1
-git push >> git_log.txt 2>&1
+git push --set-upstream origin main >> git_log.txt 2>&1
 echo ===== done ===== >> git_log.txt
 
 echo.
